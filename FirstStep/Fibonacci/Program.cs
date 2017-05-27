400846bd6c08a2de5a34e00e51483dce6997955e
vanhakobyan <van19962013@mail.ru>
2017-05-27 18:21:00 +0400

    add fibonacci and sum digits

---
+++ FirstStep/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static long fib(int n)
+        {
+            if (n <= 2)
+                return 1;
+            return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(45));
+        }
+    }
+}
---
+++ FirstStep/One100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One100
+{
+    class Program
+    {
+        static int sum(int x)
+        {
+            if (x < 10)
+            {
+                return 1;
+            }
+            return 1 + sum(x / 10);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(200));
+        }
+    }
+}
