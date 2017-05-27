cfeb67f3024e7454da904fb56fc533649e6859c3
vanhakobyan <van19962013@mail.ru>
2017-05-27 17:27:13 +0400

    add :one:

---
+++ FirstStep/FirstStep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStep
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n < 2) return 1;
+            return n * factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(factorial(n));
+            } while (true);
+        }
+    }
+}
