eacc2eb21305fe3c37276b738bdfd6398edbc05a
vanhakobyan <van19962013@mail.ru>
2017-05-27 17:47:38 +0400

    add backword :two: :v:

---
+++ FirstStep/StringsBookWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsBackWords
+{
+    class Program
+    {
+        static void BookWord(string s)
+        {
+            if (s.Length >= 1)
+            {
+                string s1 = s.Substring(1, s.Length - 1);
+                BookWord(s1);
+                Console.Write(s[0]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            BookWord("armen");
+            do
+            {
+            } while (true);
+        }
+    }
+}
