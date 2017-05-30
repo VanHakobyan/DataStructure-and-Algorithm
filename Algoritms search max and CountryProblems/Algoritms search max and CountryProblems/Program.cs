dba57eabc8a7922ba25e352f94b3ed46490dc555
vanhakobyan <van19962013@mail.ru>
2017-05-30 18:05:57 +0400

    add country and search algorithm

---
+++ Algoritms search max and CountryProblems/Algoritms search max and CountryProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLessons
+{
+    class Program
+    {
+        static int maxElem(int[] array)
+        {
+            if (array.Length == 1)
+            {
+                return array[0];
+            }
+            int i = 0;
+            int[] first = new int[array.Length / 2];
+            int[] second = new int[array.Length - first.Length];
+            for( ; i < first.Length; i++)
+            {
+                first[i] = second[i];
+            }
+            for (int k = 0; k < second.Length; k++)
+            {
+                second[k] = array[i++];
+            }
+            return Math.Max(maxElem(first), maxElem(second));
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(maxElem(new int[] { 12, 51, 25, 14, 16, 10}));
+        }
+    }
+}
---
+++ Algoritms search max and CountryProblems/CountryBrnapet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryBrnapet
+{
+    class Program
+    {
+        static int CountryArdz(int n, int k)
+        {
+            if (k > n) return 0;
+            if (k == n || k == 0) return 1;
+            else
+                return CountryArdz(n - 1, k - 1) +
+                 CountryArdz(n - 1, k);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountryArdz(3,2));
+        }
+    }
+}
