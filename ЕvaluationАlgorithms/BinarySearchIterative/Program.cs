d95eebe18679cf4c1dc8ea66cd81d4a4df15f857
vanhakobyan <van19962013@mail.ru>
2017-06-01 18:22:40 +0400

    add binary sort alg iteration

---
+++ ЕvaluationАlgorithms/BinarySearchIterative/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchIterative
+{
+    class Program
+    {
+        static int BinSearch(int[] a, int n)
+        {
+
+            int first = 0;
+            int midle = a.Length / 2;
+            int last = a.Length - 1;
+            while (last >= first)
+            {
+                if (n == a[midle])
+                {
+                    return midle;
+                }
+                if (n > a[midle])
+                {
+                    first = midle + 1;
+                }
+                else
+                {
+                    last = midle - 1;
+                }
+                midle = first + (last - first) / 2;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 5, 15, 48, 125, 131, 185 };
+            Console.WriteLine(BinSearch(a, 185));//last
+            Console.WriteLine(BinSearch(a, 2));//first
+            Console.WriteLine(BinSearch(a, 48));//midle
+            Console.WriteLine(BinSearch(a, 8));//404
+
+        }
+    }
+}
