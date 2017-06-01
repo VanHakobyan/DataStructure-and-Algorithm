c76db8255709fed1a4f11da550c1b2b943e53c3b
vanhakobyan <van19962013@mail.ru>
2017-06-01 18:56:08 +0400

    add selection sort algoritm :ok:

---
+++ ЕvaluationАlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static int Max(int[] a, int t)
+        {
+            int maxIndex = 0;
+            for (int i = 0; i < a.Length - t; i++)
+            {
+                if (a[maxIndex] < a[i])
+                {
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int maxElem = Max(array, i);
+                Swap(ref array[maxElem], ref array[array.Length - 1 - i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 14, 2, 19, 54 };
+            Sort(array);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write(array[i]+",");
+            }
+        }
+    }
+}
