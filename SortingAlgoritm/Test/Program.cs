d038d0eb8468e0ac4e19034f316b0723be63fa9b
VanHakobyan <van19962013@mail.ru>
2017-06-10 00:36:48 +0400

    add sorting algoritm Lib and test App :v:

---
+++ SortingAlgoritm/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using SortingLibrary;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Random rn = new Random();
+            int[] array = new int[rn.Next(0, 100)];
+            Console.WriteLine("Before Sort \n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rn.Next(0, 100);
+                Console.Write(array[i]+" ");
+            }
+            Console.WriteLine("\n");
+
+            //UnComment one items
+
+            Bubble.BubbleSort(ref array);
+            //Insertion.InsertionSort(array);
+            //Merge.MegreSort(array);
+            //Quick.QuickSort(array);
+            //Selection.SelectionSort(array);
+
+            Console.WriteLine("After Sort \n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
